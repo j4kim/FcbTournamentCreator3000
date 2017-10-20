8b1a79bca1892b887a2a708848d146759ba1d745
joaquim <jivkim@gmail.com>
2017-10-20 16:13:44 +0200

    ajout des catégories par handlebars

--- form.js
+++ form.js
@@ -1,5 +1,8 @@
 $(function(){
-    var pauseTemplate = Handlebars.compile(
+
+    // PAUSES
+
+    let pauseTemplate = Handlebars.compile(
         $("#pause-template").html()
     );
 
@@ -19,4 +22,26 @@ $(function(){
             $(".pauses-container").addClass("d-none")
         }
     });
+
+
+    // CATEGORIES
+
+    let categoryTemplate = Handlebars.compile(
+        $("#category-template").html()
+    );
+
+    function addCategory(data){
+        $("#addCategory").before(categoryTemplate(data));
+    }
+
+    $("#addCategory").click(addCategory);
+
+    $("form").on("click", ".removeCategory", e => {
+        $(e.target).closest(".category").remove();
+    });
+
+    // OPEN JSON FILE
+
+
+
 });
