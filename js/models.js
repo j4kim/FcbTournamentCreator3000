b5b85ce1b48a3459805b81ded5e4c7e8fbadb414
joaquim <jivkim@gmail.com>
2017-10-24 16:41:56 +0200

    création de la liste des matchs par tour

--- js/models.js
+++ js/models.js
@@ -10,9 +10,11 @@ class Team{
 }
 
 class Match{
-    constructor(teamA, teamB){
-        this.teamA = teamA;
-        this.teamB = teamB;
+    constructor(teams){
+        // randomize first/second
+        let r = Math.random() > .5 ? 0 : 1;
+        this.teamA = teams[r];
+        this.teamB = teams[1-r];
     }
 
     get priority(){
@@ -21,32 +23,62 @@ class Match{
 }
 
 class Round{
+    constructor(teams){
+        let matchs = Round.makeMatchList(teams);
+        console.log(matchs);
+        this.schedule = this.makeSchedule(matchs);
+    }
 
+    static makeMatchList(teams) {
+        let matchs = [];
+        for (let i = 0; i < teams.length; i++) {
+            for (let j = i + 1; j < teams.length; j++) {
+                matchs.push(new Match([teams[i], teams[j]]));
+            }
+        }
+        return shuffle(matchs);
+    }
+
+    makeSchedule(matchs) {
+        let schedule = [];
+        // empty the match list by popping the most important
+
+    }
 }
 
 class Group{
-    constructor(teams, name){
-        console.log("new group", name, teams);
+    constructor(teams, rounds, name){
+        console.log("new group", name, teams)
+        let firstRound = new Round(teams);
     }
 }
 
 class Category{
     constructor(obj){
         this.name = obj.name;
-        // group formation
-        let teams = shuffle(obj.teams, { 'copy': true });
-        let slices = new Array(obj.qualif.groups);
+        this.formGroups(obj.teams, obj.qualif.groups, obj.qualif.rounds);
+    }
+
+    formGroups(teams, groups, rounds){
+        // randomize teams array and form groups
+        teams = shuffle(teams, { 'copy': true })
+        // empty array
+        let slices = new Array(groups);
         let i = 0;
+        // distribute teams one by one until there are no more
         while(teams.length){
             if(!Array.isArray(slices[i]))
                 slices[i] = [];
             slices[i].push(teams.pop());
             i++;
-            i %= obj.qualif.groups;
+            i %= groups;
         }
+        // create Groups from the team slices
         this.groups = [];
         slices.forEach(function(slice, index){
-            this.groups.push(new Group(slice, this.name +" "+ (index+1)));
+            this.groups.push(
+                new Group(slice, rounds, this.name +" "+ (index+1))
+            );
         }, this);
     }
 }
