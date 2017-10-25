91afd77b47e9abb366f243d9b9fad39e8843aec6
joaquim <jivkim@gmail.com>
2017-10-25 13:40:14 +0200

    génération du programme d'un tour d'un groupe

--- js/models.js
+++ js/models.js
@@ -8,21 +8,33 @@ class Team{
         return this.waiting - this.played;
     }
 
+    playVirtually(){
+        this.played++;
+        this.waiting=0;
+    }
+
     toString(){
-        return this.name;
+        return this.name + " (" + this.waiting + " - " + this.played + ")";
     }
 }
 
 class Match{
-    constructor(teams){
+    constructor(teams, group){
         // randomize first/second
         let r = Math.random() > .5 ? 0 : 1;
         this.teamA = teams[r];
         this.teamB = teams[1-r];
+        this.group = group;
     }
 
     get priority(){
         return this.teamA.priority + this.teamB.priority;
+    }
+
+    playVirtually(){
+        this.group.wait();
+        this.teamA.playVirtually();
+        this.teamB.playVirtually();
     }
 
     static compare(m1, m2){
@@ -33,7 +45,10 @@ class Match{
         let t1 = new Team("t1");
         let t2 = new Team("t2");
         let t3 = new Team("t3");
-        let matchs = Round.makeMatchList([t1,t2,t3]);
+        let m1 = new Match([t1,t2]);
+        let m2 = new Match([t1,t3]);
+        let m3 = new Match([t2,t3]);
+        let matchs = [m1,m2,m3];
         t1.played = 12;
         t2.waiting = 42;
         console.log("before sort");
@@ -44,7 +59,9 @@ class Match{
         console.log("after");
         console.log(matchs[0].toString()); // should be t2-t3 (42)
         console.log(matchs[1].toString()); // t2-t1 (30)
-        console.log(matchs[2].toString()); // t3-t1 (-13)
+        console.log(matchs[2].toString()); // t3-t1 (-12)
+        let chosen = matchs.shift();
+        return chosen === m3;
     }
 
     toString(){
@@ -52,34 +69,42 @@ class Match{
     }
 }
 
-class Round{
-    constructor(teams){
-        let matchs = Round.makeMatchList(teams);
-        console.log(matchs);
-        this.schedule = this.makeSchedule(matchs);
+class Group{
+    constructor(teams, rounds, name){
+        this.teams = teams;
+        console.log("New Group", name, teams);
+
+        let matchs = this.makeMatchList(teams);
+        this.firstRound = this.makeFirstRound(matchs);
+        console.log("firstRound", this.firstRound);
+
+        // let firstRound = new Round(teams);
     }
 
-    static makeMatchList(teams) {
+    wait(){
+        this.teams.forEach(team => team.waiting++);
+    }
+
+    makeMatchList(teams) {
         let matchs = [];
         for (let i = 0; i < teams.length; i++) {
             for (let j = i + 1; j < teams.length; j++) {
-                matchs.push(new Match([teams[i], teams[j]]));
+                matchs.push(new Match([teams[i], teams[j]], this));
             }
         }
         return shuffle(matchs);
     }
 
-    makeSchedule(matchs) {
+    makeFirstRound(matchs) {
         let schedule = [];
         // empty the match list by popping the most important
-
-    }
-}
-
-class Group{
-    constructor(teams, rounds, name){
-        console.log("new group", name, teams)
-        let firstRound = new Round(teams);
+        while(matchs.length){
+            matchs.sort(Match.compare);
+            let chosenMatch = matchs.shift();
+            chosenMatch.playVirtually();
+            schedule.push(chosenMatch);
+        }
+        return schedule;
     }
 }
 
