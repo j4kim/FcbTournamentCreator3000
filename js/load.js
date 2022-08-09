dd615b2c051684c99a141a9f4d4269ff0127c8a4
Joaquim Perez <jivkim@gmail.com>
2022-08-09 23:39:13 +0200

    redundant code

--- js/load.js
+++ js/load.js
@@ -35,7 +35,6 @@ function getConfig(){
 function getJson(){
     let j = {};
     j.name = $("#name").val();
-    j.config = getConfig();
     if(SCHEDULE){
         j.schedule = SCHEDULE;
         j.config = CONFIG;
