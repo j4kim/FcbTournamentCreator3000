8aeb5892a808402d16766071020b95408ad429d1
Joaquim Perez <jivkim@gmail.com>
2022-08-09 23:33:37 +0200

    hide real admin hash password once admin mode is activated

--- js/load.js
+++ js/load.js
@@ -102,6 +102,7 @@ $(function(){
 
     if (hashedPassword === hashedHash) {
         $("body").addClass("admin")
+        window.location.hash = "#admin"
     }
 
     if (window.location.hash === "#ranking") {
